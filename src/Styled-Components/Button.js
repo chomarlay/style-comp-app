8f645eab397529bb00ea742b6a888a70051a5e9b
chomarlay <chomarlay.aung@gmail.com>
2020-06-06 21:00:50 +1000

    initial load

---
+++ src/Styled-Components/Button.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Button = styled.a`
+  background: white;
+  color: ${(props) => (props.themeColor ? props.themeColor : 'yellow')};
+  font-size: 2em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid black;
+  border-radius: 5px;
+`;
+
+export default Button;
---
+++ src/Styled-Components/Heading1.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Heading1 = styled.h1`
+  background: white;
+  color: black;
+  font-size: 3em;
+  margin: 1em;
+  text-align: center;
+`;
+
+export default Heading1;
